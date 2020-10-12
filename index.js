3d59ba18cbebaf4ea128dfa9b839b32f38147c28
Hitesh Sharma <hitesharma3899@gmail.com>
2020-10-12 11:06:57 +0530

    Code Refactor

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const puppeteer = require('puppeteer');
 
 var url = 'https://www.amazon.in/Samsung-Galaxy-Storage-Additional-Exchange/dp/B089MQ7C7V/ref=sr_1_5?dchild=1&qid=1601798136&s=electronics&sr=1-5';
 
-async function main() {
+async function config() {
     const browser = await puppeteer.launch();
     let page = await browser.newPage();
     await page.goto(url);
+    return page;
+}
+
+async function trackPrice(page) {
+    await page.reload();
     let text = await page.evaluate(() => {
         return document.querySelector('#priceblock_ourprice').innerText;
     })
     console.log(text);
-    await browser.close();
+}
+
+async function main() {
+    const page = await config();
+    trackPrice(page);
 }
 
 main();
