b6c9f06af295979913573f5b69e059d70242abbf
Hitesh Sharma <hitesharma3899@gmail.com>
2020-10-12 12:40:17 +0530

    added user input support

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const puppeteer = require('puppeteer');
 const CronJob = require('cron').CronJob;
+const prompts = require('prompts');
 
-var url = 'https://www.amazon.in/Samsung-Galaxy-Storage-Additional-Exchange/dp/B089MQ7C7V/ref=sr_1_5?dchild=1&qid=1601798136&s=electronics&sr=1-5';
-
-async function config() {
+async function config(url) {
     const browser = await puppeteer.launch();
     let page = await browser.newPage();
     await page.goto(url);
@@ -18,12 +17,22 @@ async function trackPrice(page) {
     console.log(text);
 }
 
-async function main() {
-    const page = await config();
+async function schedule(page) {
     let job = new CronJob('*/15 * * * * *', () => {
         trackPrice(page);
     }, null, true, null, null, true);
     await job.start();
+}
+
+async function main() {
+    const res = await prompts({
+        type: 'text',
+        name: 'url',
+        message: 'Enter Product url to monitor: ',
+
+    });
+    const page = await config(res.url);
+    schedule(page);
 }
 
 main();
