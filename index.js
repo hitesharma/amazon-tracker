1143079d79dcb641f1cc2a545c1041f24d3d9e0d
Hitesh Sharma <hitesharma3899@gmail.com>
2020-10-16 08:15:19 +0530

    support for multiple websites

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function config(url) {
     return page;
 }
 
-async function trackPrice(page) {
+async function trackAmazon(page) {
     await page.reload();
     let text = await page.evaluate(() => {
         return document.querySelector('#priceblock_ourprice').innerText;
@@ -17,22 +17,43 @@ async function trackPrice(page) {
     console.log(text);
 }
 
-async function schedule(page) {
+async function trackFlipkart(page) {
+    await page.reload();
+    let text = await page.evaluate(() => {
+        return document.querySelector('div._1vC4OE._3qQ9m1').innerText;
+    })
+    console.log(text)
+}
+
+async function schedule(page, platform) {
     let job = new CronJob('*/15 * * * * *', () => {
-        trackPrice(page);
+        switch (platform) {
+            case 1:
+                trackAmazon(page);
+                break;
+            case 2:
+                trackFlipkart(page);
+                break;
+        }
     }, null, true, null, null, true);
     await job.start();
 }
 
 async function main() {
-    const res = await prompts({
-        type: 'text',
-        name: 'url',
-        message: 'Enter Product url to monitor: ',
+    const res = await prompts([
+        {
+            type: 'number',
+            name: 'platform',
+            message: '1) Amazon\n  2)Flipkart\n'
 
-    });
+        },
+        {
+            type: 'text',
+            name: 'url',
+            message: 'Enter Product url to monitor: ',
+        }]);
     const page = await config(res.url);
-    schedule(page);
+    schedule(page, res.platform);
 }
 
 main();
